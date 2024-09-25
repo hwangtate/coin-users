221d2fc8fb8d102a01779ce9bd69f388dfe3d1b3
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-25 15:55:23 +0900

    Test: add register api test code

--- users/tests/test_api_user.py
+++ users/tests/test_api_user.py
@@ -1,5 +1,21 @@
+from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
+
+from users.models import User
 
 
 class APIUserTestCase(APITestCase):
-    def setUp(self): ...
+    def setUp(self):
+        self.url = reverse("user_register")
+        self.data = {
+            "nickname": "test user",
+            "email": "test@example.com",
+            "password": "Qwea1287!",
+            "password2": "Qwea1287!",
+        }
+
+    def test_user_registration(self):
+        response = self.client.post(self.url, data=self.data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.first().nickname, "test user")
